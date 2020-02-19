e65b748deb34db5c4f46b6f149cf6eb7f57d0329
yonureker <yonureker@gmail.com>
2020-02-19 15:08:25 -0800

    new removedups solution

--- removeDuplicatesFromSorted.js
+++ removeDuplicatesFromSorted.js
@@ -1,10 +1,16 @@
-const removeDuplicates = function(nums) {
-  for (let i = 0; i < nums.length; i++){
-      if (nums[i] === nums[i-1]){
-          nums.splice(i, 1);
-          i--;
-      }
-  }
+// const removeDuplicates = function(nums) {
+//   for (let i = 0; i < nums.length; i++){
+//       if (nums[i] === nums[i-1]){
+//           nums.splice(i, 1);
+//           i--;
+//       }
+//   }
   
-  return nums.length
+//   return nums.length
+// };
+
+
+//great solution
+var removeDuplicates = function(nums) {
+  nums.splice(0, nums.length, ...(new Set(nums)));
 };
