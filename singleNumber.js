a124b649a0df19541ac2d0c9bd5bdbe546529d13
yonureker <yonureker@gmail.com>
2020-03-03 13:02:09 -0800

    add mdn link

--- singleNumber.js
+++ singleNumber.js
@@ -1,5 +1,5 @@
 var singleNumber = function(nums) {
-
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Bitwise_Operators#Bitwise_XOR
   return nums.reduce((acc,cur)=> acc^cur,0)
 };
 
